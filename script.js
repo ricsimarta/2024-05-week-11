241d788a086c8481f16f8327f3b7880479324728
ricsimarta <ricsimarta@gmail.com>
2024-07-29 17:40:48 +0200

    done

--- script.js
+++ script.js
@@ -20,40 +20,20 @@ const buttonComponent = (type) => `
 const makeDomFromData = (data, rootElement) => {
   rootElement.innerHTML = "";
 
-  console.log(data);
   if (data.info.next) rootElement.insertAdjacentHTML("beforeend", buttonComponent("next"));
   if (data.info.prev) rootElement.insertAdjacentHTML("beforeend", buttonComponent("prev"));
 
   const buttons = document.querySelectorAll("button");
-  buttons.forEach(button => button.addEventListener("click", () => fetch(data.info[button.classList[0]])
-    .then(res => res.json())
-    .then(newData => makeDomFromData(newData, rootElement))
-  ));
-  
-  /* if (data.info.next) {
-    
-    const nextButtonElement = document.querySelector("button.next");
-    nextButtonElement.addEventListener("click", () => {
-      fetch(data.info.next)
+
+  buttons.forEach(button => button.addEventListener("click", () => {
+    fetch(data.info["next"])
       .then(res => res.json())
       .then(newData => makeDomFromData(newData, rootElement))
-    });
-  }
-
-
-  if (data.info.prev) {
-    
-    const prevButtonElement = document.querySelector("button.prev");
-    prevButtonElement.addEventListener("click", () => {
-      fetch(data.info.prev)
-      .then(res => res.json())
-      .then(newData => makeDomFromData(newData, rootElement))
-    });
-  } */
+  }));
 
   rootElement.insertAdjacentHTML("beforeend", charactersComponent(data.results));
 };
 
-fetch("https://rickandmortyapi.com/api/character")
+fetch("https://rickandmortyapi.com/api/character?page=2")
   .then((response) => response.json())
   .then((data) => makeDomFromData(data, document.querySelector("#root")));
